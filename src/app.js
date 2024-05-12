a068d6568a721c8fe59cf6fb9b30a055724715a8
Leivi521 <165715355+Leivi521@users.noreply.github.com>
2024-05-12 23:20:05 +0000

    ecuse genrator-Foundation completed

--- src/app.js
+++ src/app.js
@@ -4,8 +4,56 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import "./assets/img/procrastination.png";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+  generateExcuse(); // Call the function to generate and display the excuse
 };
+function generateExcuse() {
+  const who = [
+    "My dog",
+    "My grandma",
+    "The mailman",
+    "My bird",
+    "My fish",
+    "My cat",
+    "My imaginary friend",
+    "God himself"
+  ];
+  const action = [
+    "ate",
+    "peed",
+    "crushed",
+    "broke",
+    "stole",
+    "set fire to",
+    "spit on",
+    "ripped"
+  ];
+  const what = [
+    "my homework",
+    "my phone",
+    "the car",
+    "my bike",
+    "my scooter",
+    "everything"
+  ];
+  const when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const randomWhoIndex = Math.floor(Math.random() * who.length);
+  const randomActionIndex = Math.floor(Math.random() * action.length);
+  const randomWhatIndex = Math.floor(Math.random() * what.length);
+  const randomWhenIndex = Math.floor(Math.random() * when.length);
+  const excuse = `<b>${who[randomWhoIndex]}</b> <b>${action[randomActionIndex]}</b> <b>${what[randomWhatIndex]}</b> ${when[randomWhenIndex]}.`;
+
+  const excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = excuse;
+  return excuse;
+}
